4912692db377d57ec32b9cf0c474623aa5385ef0
villetaro <ville.t.aro@helsinki.fi>
2017-01-28 22:17:10 +0200

    Harkittuja muutoksia hähää

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -3,6 +3,10 @@
 
 # Data Wranling, still chillin'
 
+install.packages("dplyr")
+library(dplyr)
+
+
 lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
 dim(lrn14)
 str(lrn14)
@@ -10,14 +14,30 @@ str(lrn14)
 # Data appears to have 183 observation of 60 variable. Lets look further :)
 # With structure we can learn the different variables.
 
-Analysis <- c("gender","age","attitude", "deep", "stra", "surf", "points")
-lrn14 <- lrn14[ which(points > 0),]
-str(lrn14)
-lrn14 <- lrn14[ which(points > 0),]
-newdata <- lrn14[ which(lrn14$"points">0)]
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30", "D06",  "D15", "D23", "D31")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+
+# select the columns related to deep learning and create column 'deep' by averaging
+deep_columns <- select(lrn14, one_of(deep_questions))
+lrn14$deep <- rowMeans(deep_columns)
+
+# select the columns related to surface learning and create column 'surf' by averaging
+surface_columns <- select(lrn14, one_of(surface_questions))
+lrn14$surf <- rowMeans(surface_columns)
+
+# select the columns related to strategic learning and create column 'stra' by averaging
+strategic_columns <- select(lrn14, one_of(strategic_questions))
+lrn14$stra <- rowMeans(strategic_columns)
+
+# choose a handful of columns to keep
+keep_columns <- c("gender","Age","Attitude", "deep", "stra", "surf", "Points")
+
+# select the 'keep_columns' to create a new dataset
+learning2014 <- select(lrn14, one_of(keep_columns))
+
+# select rows where points is greater than zero
+learning2014 <- filter(learning2014, Points > 0)
 
 # see the structure of the new dataset
-
-str(Analysis)
-str(lrn14)
-
+str(learning2014)
